9a860bc83714f1eddbf82fe0100a99375be17771
Elena Rentero <elenaclementerentero@gmail.com>
2022-11-28 08:44:43 +0100

    add template react and pubish github pages

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import '../styles/App.scss';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USEEFFECT
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR
+
+  //RETURN
+  
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
