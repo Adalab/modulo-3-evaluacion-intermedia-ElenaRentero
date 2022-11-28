ff1431d5859264659fad4714bfc6e0bee64f4878
Elena Rentero <elenaclementerentero@gmail.com>
2022-11-28 11:08:13 +0100

    add some styles

--- src/components/App.js
+++ src/components/App.js
@@ -78,21 +78,22 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1>Adalabers</h1>
+      <header className="header">
+        <h1 className="header__title">Adalabers</h1>
       </header>
-      <section>
-        <form>
-        <label htmlFor='searchName'>Nombre:</label>
+      <section className="section">
+        <form className="section__form">
+        <label className="section__form--label" htmlFor='searchName'>Nombre:</label>
         <input
+              className="section__form--input"
               name='search'
               id='search'
               type='search'
               placeholder="Ej:Maricarmen"
               onInput={handleSearchName}
             />
-        <label htmlFor='searchCounselor'>Escoge una tutora:</label>
-        <select name='counselor' id='searchCounselor' onChange={handleSearchCounselor}>
+        <label className="section__form--label" htmlFor='searchCounselor'>Escoge una tutora:</label>
+        <select className="section__form--select" name='counselor' id='searchCounselor' onChange={handleSearchCounselor}>
           <option selected disabled default>Escoge una opción</option>
           <option value='Yanelis'>Yanelis</option>
           <option value='Dayana'>Dayana</option>
@@ -100,42 +101,45 @@ function App() {
         </select>
         </form>
       </section>
-      <section>
-        <table className="table">
-          <thead><tr>
+      <section className="section">
+        <table className="section__table">
+          <thead className="section__table--head"><tr>
             <th>Nombre</th>
             <th>Tutora</th>
             <th>Especialidad</th>
             <th>Redes</th>
           </tr></thead>
-          <tbody>{renderAdalabers()}</tbody>
+          <tbody className="section__table--body">{renderAdalabers()}</tbody>
         </table>
-        <form action='' onSubmit={handleSubmit}>
-            <label htmlFor='name'>Nombre:</label>
+        <form className="section__form" action='' onSubmit={handleSubmit}>
+            <label className="section__form--label" htmlFor='name'>Nombre:</label>
             <input
+              className="section__form--input"
               name='name'
               id='name'
               type='text'
               value={newAdalaber.name}
               onChange={handleInput}
             />
-            <label htmlFor='counselor'>Tutora:</label>
+            <label className="section__form--label" htmlFor='counselor'>Tutora:</label>
             <input
+              className="section__form--input"
               name='counselor'
               id='counselor'
               type='text'
               value={newAdalaber.counselor}
               onChange={handleInput}
             />
-            <label htmlFor='speciality'>Especialidad:</label>
+            <label className="section__form--label" htmlFor='speciality'>Especialidad:</label>
             <input
+              className="section__form--input"
               name='speciality'
               id='speciality'
               type='text'
               value={newAdalaber.speciality}
               onChange={handleInput}
             />
-            <button type='button' onClick={handleClick}>
+            <button className="section__form--button" type='button' onClick={handleClick}>
               Añadir una nueva Adalaber
             </button>
           </form>
