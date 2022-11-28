3d0ffa9caad1f6927547c78b21aaaaf6a465781b
Elena Rentero <elenaclementerentero@gmail.com>
2022-11-28 09:43:23 +0100

    add fetch api and render all Adalabers

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,59 @@
 import '../styles/App.scss';
+import fetchData from '../services/api';
+import { useState, useEffect } from 'react';
+
 
 function App() {
   // VARIABLES ESTADO
+  const [adalabers, setAdalabers] = useState([]);
+  const [newAdalaber, setNewAdalaber] = useState({
+    id: crypto.randomUUID(),
+    name: '',
+    counselor: '',
+    speciality: '',
+  });
+
 
   // USEEFFECT
+  useEffect(() => {
+    fetchData().then((adalaber) => {
+      setAdalabers(adalaber)
+    })
+  }, []);
 
   // FUNCIONES HANDLER
 
   // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR
+  const renderAdalabers = () => {
+      const adalabersResult = adalabers.map((eachAdalaber) => {
+        return (
+          <tr key={eachAdalaber.id}>
+            <td>{eachAdalaber.name}</td>
+            <td>{eachAdalaber.counselor}</td>
+            <td>{eachAdalaber.speciality}</td>
+          </tr>
+        );
+      });
+      return adalabersResult;
+    };
 
   //RETURN
-  
+
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <header>
+        <h1>Adalabers</h1>
+      </header>
+      <section>
+        <table className="table">
+          <thead><tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr></thead>
+          <tbody>{renderAdalabers()}</tbody>
+        </table>
+      </section>
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+function fetchData() {
+    return fetch ('https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/adalabers-v1/promo-radsajsd.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const result = data.results.map((eachData) => {
+            const adalaber = {
+            id: eachData.id,
+            name: eachData.name,
+            counselor: eachData.counselor,
+            promo: eachData.promo,
+            speciality: eachData.speciality,
+            };
+            return adalaber;
+        });
+        return result
+    });
+};   
+
+export default fetchData;
